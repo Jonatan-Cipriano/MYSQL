9ae1c1f5fe7116d64146ed7d322d2f15dd0f5374
Jonatan-Cipriano <jony.12333@gmail.com>
2022-04-18 23:55:10 -0600

    TAREA de sql server

---
+++ Tarea Progra para lunes.sql
@@ -0,0 +1,11 @@
+select * from marcas;
+
+INSERT INTO `bd_crud`.`marcas` (`marca`) 
+VALUES ('SARA');
+
+SELECT * FROM productos;
+INSERT INTO `bd_crud`.`productos` (`producto`, `descripcion`, `precio_costo`, `precio_venta`, `existencia`, `fecha_ingreso`) 
+VALUES ('Pantalon', 'Pantalon de lona acampanado color azul', '75.99', '149.99', '200', '2022-01-01');
+
+
+
